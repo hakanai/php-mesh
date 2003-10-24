13924a3a1b77f0a6670fff2a84b536a040bfa91b
Trejkaz <trejkaz@trypticon.org>
2003-10-24 09:23:40 +0000

    Add the basic example

---
+++ example/default.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>MySite :: <?php $page->printTitle(); ?></title>
+<?php $page->printHeadRemains(); ?>
+</head>
+<body>
+
+<h1><?php $page->printTitle(); ?></h1>
+
+<?php $page->printBody(); ?>
+
+</body>
+</html>
+
